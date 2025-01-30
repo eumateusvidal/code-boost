7d57b9e8baa1f74acce67e7e6cc6398b595f0118
Mateus Vidal <devmateusvidal@gmail.com>
2025-01-30 08:04:57 -0300

    January 30

---
+++ 09-lp-bankboost-wordpress/app/public/wp-content/themes/theme-lp-bankboost/index.php
@@ -0,0 +1,472 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- Primary Meta Tags -->
+  <title>Bankboost -  Abra sua conta, é só baixar o app!</title>
+  <meta name="title" content="Bankboost -  Abra sua conta, é só baixar o app!" />
+  <meta name="description" content="Desperte seu potencial financeiro com o Bankboost!" />
+
+  <!-- Open Graph / Facebook -->
+  <meta property="og:type" content="website" />
+  <meta property="og:url" content="https://bankboost-matt.netlify.app/" />
+  <meta property="og:title" content="Bankboost -  Abra sua conta, é só baixar o app!" />
+  <meta property="og:description" content="Desperte seu potencial financeiro com o Bankboost!" />
+  <meta property="og:image" content="./img/00. SEO/share-og.jpg" />
+
+
+  <!-- Twitter -->
+  <meta property="twitter:card" content="summary_large_image" />
+  <meta property="twitter:url" content="https://bankboost-matt.netlify.app/" />
+  <meta property="twitter:title" content="Bankboost -  Abra sua conta, é só baixar o app!" />
+  <meta property="twitter:description" content="Desperte seu potencial financeiro com o Bankboost!" />
+  <meta property="twitter:image" content="./img/00. SEO/share-og.jpg" />
+
+  <!-- Meta Tags Generated with https://metatags.io -->
+  <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+  <link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(  )?>/css/main.css">
+  <link rel="icon" type="img/png" href="./img/00. SEO/favicon.png">
+</head>
+<body>
+
+  <header>
+    <div class="container">
+      <a href="#">
+        <img src="./img/01. header/Vector.svg" alt="logo bank boost">
+      </a>
+      <div class="right">
+        <nav>
+          <a href="#">Produtos Boost</a>
+          <a href="#">Conta digital PJ</a>
+          <a href="#">Quem somos</a>
+          <a href="#">Blog</a>
+          <a href="#">Ajuda</a>
+        </nav>
+        <button class="btn">Abra sua conta digital</button>
+      </div>
+      <button>
+        <img src="./img/01. header/btn-mobile.svg" alt="" class="btn-mobile">
+      </button>
+    </div>
+  </header>
+  
+ <section class="s-hero">
+    <div class="container">
+      <div class="left" data-aos="fade-right" data-aos-duration="1500" data-aos-easing="ease">
+        <h3>
+          Abra sua conta, é só baixar o app!
+        </h3>
+        <h1>
+          Desperte seu potencial financeiro com o Bankboost!
+        </h1>
+        <button class="btn">
+          Abra sua conta digital
+        </button>
+        <ul>
+          <li>
+            <img src="./img/02. hero-section/anuidade-icon.svg" alt="ícone cartão">
+            <span>Cartão sem<br>anuidade</span>
+          </li>
+          <li>
+            <img src="./img/02. hero-section/conta-gratis-icon.svg" alt="Ícone celular">
+            <span>Conta digital <br>100% grátis</span>
+          </li>
+          <li>
+            <img src="./img/02. hero-section/dinheiro-icon.svg" alt="Ícone dinheiro">
+            <span>Seu dinheiro <br>rendendo mais</span>
+          </li>
+        </ul>
+      </div>
+      <div class="right">
+        <h2 data-aos="zoom-in" data-aos-duration="1000" data-aos-easing="ease-in-sine" data-aos-delay="400">
+          Banco 100% digital
+        </h2>
+        <div class="cards">
+          <img src="./img/02. hero-section/card-hero-01.png" alt="imagem de um cartão" class="item card-one" data-aos="fade-down" data-aos-duration="1000" data-aos-easing="ease-in-sine" data-aos-delay="300">
+          <img src="./img/02. hero-section/card-hero-02.png" alt="imagem de um cartão" class="item card-two" data-aos="fade-up" data-aos-duration="1000" data-aos-easing="ease-in-sine" data-aos-delay="300">
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+  <section class="s-boost">
+    <div class="container">
+      <div class="left" data-aos="fade-up" data-aos-duration="1000" data-aos-easing="ease-in-sine">
+        <div class="mockup">
+          <img src="./img/03. boost-section/mockup-iphone.svg" alt="mockup-iphone">
+        </div>
+        <div class="description">
+          <img src="./img/03. boost-section/icon-app-boost.svg" alt="icon-app-boost" class="icon">
+          <h3>
+            Baixe nosso app
+          </h3>
+          <p>
+            Que tal abrir uma conta digital para ver como a gente faz a sua vida muito mais simples?
+          </p>
+          <ul>
+            <li>
+              <a href="#" target="_blank">
+                <img src="./img/03. boost-section/apple-store.svg" alt="apple-store">
+              </a>
+            </li>
+            <li>
+              <a href="#" target="_blank">
+                <img src="./img/03. boost-section/google-play.svg" alt="google-play">
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="right" data-aos="fade-left" data-aos-duration="1000" data-aos-easing="ease-in-sine">
+        <h2>
+          Resolva sua vida direto pelo app boost!
+        </h2>
+        <ul class="list">
+          <li>
+            <div class="icon">
+              <img src="./img/03. boost-section/icon-cartao.svg" alt="">
+            </div>
+            <div class="text">
+              <h4>
+                Cartão de crédito sem anuidade
+              </h4>
+              <p>
+                Conta digital com cartão de crédito sem anuidade e sem complicação
+              </p>
+            </div>
+          </li>
+          <li>
+            <div class="icon">
+              <img src="./img/03. boost-section/icon-taxas.svg" alt="">
+            </div>
+            <div class="text">
+              <h4>
+                Sem taxas
+              </h4>
+              <p>
+                Transferências, boletos de depósito e outros serviços gratuitos
+            </div>
+          </li>
+          <li>
+            <div class="icon">
+              <img src="./img/03. boost-section/icon-investimentos.svg" alt="">
+            </div>
+            <div class="text">
+              <h4>
+                Mais investimentos
+              </h4>
+              <p>
+                Rendem mais que a poupança e você resgata quando quiser
+            </div>
+          </li>
+        </ul>
+        <a href="#" class="btn">
+          Conheça outros produtos
+        </a>
+        <div class="box" data-aos="fade-up" data-aos-duration="500" data-aos-easing="ease-in-sine">
+          <div class="text">
+            <h3>
+              Boost Pejota
+            </h3>
+            <h5>
+              Contas digitais PJ gratuitas para decolar seu negócio!
+            </h5>
+            <p>
+              As melhores contas para fazer pagamentos, compras e receber dos seus clientes.
+            </p>
+            <ul>
+              <li>
+                <a href="#">
+                  Sou MEI
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  SOU ME
+                </a>
+              </li>
+            </ul>
+          </div>
+          <img src="./img/03. boost-section/card-front-pj.png" alt="cartao pj" class="card">
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="s-depositions" data-aos="fade-up" data-aos-duration="1000" data-aos-easing="ease-in-sine">
+    <div class="container">
+      <div class="title">
+        <h2>Prático, fácil, moderno 
+          <strong>Você resolve tudo sem estresse 😊</strong>
+        </h2>
+        <div class="swiper-pagination">
+          
+        </div>
+      </div>
+
+      <div class="slide-deposition">
+        <div class="swiper-wrapper">
+          <div class="swiper-slide">
+            <div class="card-deposition">
+              <div class="user">
+                <span> @marciogurka</span>
+                <img src="./img/04. deposition-section/icon-twitter.svg" alt="icon twitter">
+              </div>
+              <p>Valeu @banconenon! Linda embalagem e o cartão tbm é lindo! Muito amor por esse banco #bancoBankboost</p>
+            </div>
+          </div>
+          <div class="swiper-slide">
+            <div class="card-deposition">
+              <div class="user">
+                <span> @marciogurka</span>
+                <img src="./img/04. deposition-section/icon-twitter.svg" alt="icon twitter">
+              </div>
+              <p>Valeu @banconenon! Linda embalagem e o cartão tbm é lindo! Muito amor por esse banco #bancoBankboost</p>
+            </div>
+          </div>
+          <div class="swiper-slide">
+            <div class="card-deposition">
+              <div class="user">
+                <span> @marciogurka</span>
+                <img src="./img/04. deposition-section/icon-twitter.svg" alt="icon twitter">
+              </div>
+              <p>Valeu @banconenon! Linda embalagem e o cartão tbm é lindo! Muito amor por esse banco #bancoBankboost</p>
+            </div>
+          </div>
+          <div class="swiper-slide">
+            <div class="card-deposition">
+              <div class="user">
+                <span> @marciogurka</span>
+                <img src="./img/04. deposition-section/icon-twitter.svg" alt="icon twitter">
+              </div>
+              <p>Valeu @banconenon! Linda embalagem e o cartão tbm é lindo! Muito amor por esse banco #bancoBankboost</p>
+            </div>
+          </div>
+          <div class="swiper-slide">
+            <div class="card-deposition">
+              <div class="user">
+                <span> @marciogurka</span>
+                <img src="./img/04. deposition-section/icon-twitter.svg" alt="icon twitter">
+              </div>
+              <p>Valeu @banconenon! Linda embalagem e o cartão tbm é lindo! Muito amor por esse banco #bancoBankboost</p>
+            </div>
+          </div>
+        </div>
+      </div> 
+    </div>
+  </section>
+  
+  
+  <div class="s-account">
+    <div class="container">
+      <div class="left" data-aos="fade-right" data-aos-duration="1000" data-aos-easing="ease-in-sine">
+        <h2>
+          Aproveite <strong>Domine o seu dinheiro com uma conta 100% digital</strong>
+        </h2>
+        <ul>
+          <li>
+            <img src="./img/05. account-section/icon-cartao.svg" alt="icon-cartao">
+            <div class="text">  
+              <h4>
+                Cartão visa internacional
+              </h4>
+              <p>
+                Tenha facilidades e benefícios para o seu dia a dia.
+              </p>
+            </div>
+          </li>
+          <li>
+            <img src="./img/05. account-section/icon-mensalidade.svg" alt="icon-cartao">
+            <div class="text">  
+              <h4>
+                Zero mensalidade e anuidade
+              </h4>
+              <p>
+                Não gaste grana pagando taxas desnecessárias.
+              </p>
+            </div>
+          </li>
+          <li>
+            <img src="./img/05. account-section/icon-investimento.svg" alt="icon-cartao">
+            <div class="text">  
+              <h4>
+                Investimento que rende mais que a poupança
+              </h4>
+              <p>
+                Invista o seu dinheiro de maneira mais rentável.
+              </p>
+            </div>
+          </li>
+        </ul>
+        <a href="#" class="btn">
+          Abra sua conta digital
+        </a>
+      </div>
+      <div class="right">
+        <img src="./img/05. account-section/mockup-app-01.png" class="mockup one" alt="mockup-app-01" data-aos="fade-up" data-aos-duration="1000" data-aos-easing="ease-in-sine" data-aos-delay="300">
+        <img src="./img/05. account-section/mockup-app-02.png" class="mockup two" alt="mockup-app-02" data-aos="fade-down" data-aos-duration="1000" data-aos-easing="ease-in-sine" data-aos-delay="300">
+      </div>
+    </div>
+  </div>
+
+
+  <footer>
+    <div class="container">
+      <div class="area-social">
+        <a href="#">
+          <img src="./img/06. footer/logo-code-boost.svg" alt="">
+        </a>
+        <div class="social-media">
+          <span>Acompanhe nas redes</span>
+          <ul>
+            <li>
+              <a href="#" target="_blank">
+                <img src="./img/06. footer/icon-youtube.svg" alt="icon-youtube">
+              </a>
+            </li>
+            <li>
+              <a href="#" target="_blank">
+                <img src="./img/06. footer/icon-linkedin.svg" alt="icon-linkedin">
+              </a>
+            </li>
+            <li>
+              <a href="#" target="_blank">
+                <img src="./img/06. footer/icon-facebook.svg" alt="icon-facebook">
+              </a>
+            </li>
+            <li>
+              <a href="#" target="_blank">
+                <img src="./img/06. footer/icon-instagram.svg" alt="icon-instagram">
+              </a>
+            </li>
+            <li>
+              <a href="#" target="_blank">
+                <img src="./img/06. footer/icon-twitter.svg" alt="icon-twitter">
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="main-area">
+        <div class="left">
+          <div class="item">
+            <h5>Produtos Boost</h5>
+            <ul>
+              <li>
+                <a href="#">Cartão de crédito</a>
+              </li>
+              <li>
+                <a href="#">Cartão pré-pago</a>
+              </li>
+              <li>
+                <a href="#">Boost Mais</a>
+              </li>
+              <li>
+                <a href="#">Investimentos</a>
+              </li>
+              <li>
+                <a href="#">Empréstimo</a>
+              </li>
+            </ul>
+          </div>
+          <div class="item">
+            <h5>Conta digital PJ</h5>
+            <ul>
+              <li>
+                <a href="#">Sou MEI</a>
+              </li>
+              <li>
+                <a href="#">Sou ME</a>
+              </li>
+            </ul>
+          </div>
+          <div class="item">
+            <h5>Blog</h5>
+            <ul>
+              <li>
+                <a href="#">#focanodinheiro</a>
+              </li>
+              <li>
+                <a href="#">O poder da comunidade</a>
+              </li>
+              <li>
+                <a href="#">Desafio das 52 semanas</a>
+              </li>
+              <li>
+                <a href="#">Planilha de gastos</a>
+              </li>
+            </ul>
+          </div>
+          <div class="item">
+            <h5>Institucional</h5>
+            <ul>
+              <li>
+                <a href="#">Conheça a Bankboost</a>
+              </li>
+              <li>
+                <a href="#">Trabalhe conosco</a>
+              </li>
+              <li>
+                <a href="#">Termos de uso</a>
+              </li>
+              <li>
+                <a href="#">Políticas de privacidade</a>
+              </li>
+            </ul>
+          </div>
+          <div class="item">
+            <h5>Ajuda</h5>
+            <ul>
+              <li>
+                <a href="#">Ouvidoria</a>
+              </li>
+              <li>
+                <a href="#">Fale conosco</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="right">
+          <a href="">
+            <img src="./img/06. footer/icon-email.svg" alt="">
+            <div class="description">
+              <strong>
+                Atendimento:
+              </strong>
+              <p>oi@bankboost.com.br (24 horas)</p>
+            </div>
+          </a>
+          <a href="">
+            <img src="./img/06. footer/icon-chat.svg" alt="">
+            <div class="description">
+              <strong>
+                Imprensa:
+              </strong>
+              <p>imprensa@bankboost.com.br</p>
+            </div>
+          </a>
+        </div>
+      </div>
+      <div class="copy">
+        <div class="text">
+          <img src="./img/06. footer/emoticon.png" alt="">
+          <p>Oi! Leu até aqui? Você se preocupa com os mínimos detalhes, mesmo. A gente também. Por isso o time Bankboost está sempre trabalhando para fazer a conta digital perfeita para você ;)</p>
+        </div>
+      </div>
+    </div>
+  </footer> 
+
+  <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+  <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+  <script src="./js/scripts.js"></script>
+</body>
+</html>
