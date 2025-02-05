55f1419f5b7307b8cb931ca2acdd874e3d6ab1d3
Mateus Vidal <devmateusvidal@gmail.com>
2025-02-05 07:02:31 -0300

    February 5

--- 10-javascript/js/scripts.js
+++ 10-javascript/js/scripts.js
@@ -1,10 +1,22 @@
-let possuiCarro = true;
-let possuiMoto = true;
+// let possuiCarro = true;
+// let possuiMoto = true;
 
-if(possuiMoto) {
-  console.log('Ele tem uma moto');
-} else if (possuiCarro) {
-  console.log('Ele tem um carro');
-} else {
-  console.log('Ele não tem nenhum veículo')
+// if(possuiMoto) {
+//   console.log('Ele tem uma moto');
+// } else if (possuiCarro) {
+//   console.log('Ele tem um carro');
+// } else {
+//   console.log('Ele não tem nenhum veículo')
+// }
+
+// Funções
+
+function carroFavorito(carro) {
+  if(carro === 'Fusca') {
+    console.log('O carro favorito é o Fusca');  
+  } else if(carro === 'Gol') {
+    console.log('O carro favorito é o Gol');
+  } else {console.log('Nenhum carro é o seu favorito')}
 }
+
+carroFavorito('Golasa');
