2b92dca9721a60b51ab5ac5466cf25de8295f46c
Mateus Vidal <devmateusvidal@gmail.com>
2025-02-16 06:49:13 -0300

    February 16

--- 10-javascript/js/scripts.js
+++ 10-javascript/js/scripts.js
@@ -1,9 +1,22 @@
-const accordion = document.querySelectorAll('.js-accordion')
+const linksSection = document.querySelectorAll ('.js-nav li a');
+  function scrollToSection(event) {
+    event.preventDefault();
 
-accordion.forEach(acc => {
-    acc.addEventListener('click', () => {
-      let pai = acc.parentElement;
 
-      pai.classList.toggle('active')
+    const href = event.currentTarget.getAttribute('href');
+
+    const section = document.querySelector(href);
+
+    const initPosition = section.offsetTop;
+
+    window.scrollTo({
+      top: initPosition -80,
+      behavior: 'smooth',
     })
-})
+
+  }
+
+  linksSection.forEach(link => {
+    link.addEventListener('click', scrollToSection);
+  })
+
