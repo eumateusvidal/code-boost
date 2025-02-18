abf52e0239b2bda3ab589a7c15bbb74d4e2ed7d2
Mateus Vidal <devmateusvidal@gmail.com>
2025-02-18 08:23:44 -0300

    February 18

--- 08-bank-boost/js/scripts.js
+++ 08-bank-boost/js/scripts.js
@@ -22,3 +22,16 @@ var swiper = new Swiper(".slide-deposition", {
 });
 
   AOS.init();
+
+const btnDropdown = document.querySelector('.js-btn-dropdown');
+
+const dropdown = document.querySelector('.js-dropdown');
+
+function openDropdown(event) {
+  event.preventDefault();
+
+  dropdown.classList.toggle('active');
+}
+
+btnDropdown.addEventListener('click', openDropdown);
+btnDropdown.addEventListener('mouseleave', openDropdown);
